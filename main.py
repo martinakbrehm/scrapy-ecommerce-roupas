4962c7c82fcf5788e23ef24f56e47a7f29ac9201
gabenasci <gabenascimento1@gmail.com>
2023-09-14 10:29:18 -0300

    Add Product Title, Price, Description, URL to JSON

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
-#import requests
 import requests
+import json
 from bs4 import BeautifulSoup
 
+product_url = 'https://www.hering.com.br/camiseta-masculina-gola-v-basicos-do-brasil-4k20nmcen/p'
 
-url = 'https://www.tng.com.br'
-
-response = requests.get(url)
+response = requests.get(product_url)
 
 if response.status_code == 200:
 
     soup = BeautifulSoup(response.text, 'html.parser')
-    container = soup.find('div', class_='prateleira n12colunas')
 
+    item_list = []
 
-    # Imprima os nomes dos produtos encontrados
-    #for product in products:
-       # print(product_name.text)
+    title = soup.find('h1', class_='hering-hering-components-3-x-product-content--name').text
+
+    price_elements = soup.find('span', class_='hering-hering-components-3-x-currencyContainer')
+
+    if price_elements:
+        price = price_elements.find_all('span')
+        price_text = ''.join(span.text for span in price)
+
+    else: 
+        print('elemento de preço não encontrado')
+
+    description = soup.find('div', class_='hering-hering-components-3-x-productDescriptionValue').text
+
+    item_info = {
+        'title': title,
+        'price': price_text,
+        'description': description,
+        'url': product_url,
+    }
+    item_list.append(item_info)
+    print(item_info)
 else:
     print('Falha ao acessar a página:', response.status_code)
 
+output_file = 'products.json'
+
+with open(output_file, 'w', encoding='utf-8') as json_file:
+    json.dump(item_list, json_file, ensure_ascii=False, indent=4)
+
+print(f'\nDados salvos no arquivo {output_file}')
